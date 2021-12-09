0dd9fb5b556f71207eac011f64310b79e26d086b
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-12-10 00:42:12 +0100

    [CONF-9] added styled-components, GlobalStyles and ThemeProvider

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import GlobalStyle from 'theme/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import theme from 'theme/theme';
 
 const Root = () => (
-  <div>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <h1>Configaro</h1>
-  </div>
+    <p style={{ color: theme.colors.primary }}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+      commodi delectus esse fuga in necessitatibus nihil rem? Animi, ipsam quos?
+    </p>
+    <button type='button'>sdgdsfh</button>
+    <br />
+    <input type='text' />
+  </ThemeProvider>
 );
 
 export default Root;

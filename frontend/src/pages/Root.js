3d4d4cf01d00665971b9522c490475d51edd41e7
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-12-10 18:30:02 +0100

    [CONF-15] added default background and MainWrapper

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -2,18 +2,42 @@ import React from 'react';
 import GlobalStyle from 'theme/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 import theme from 'theme/theme';
+import MainWrapper from 'components/views/MainWrapper/MainWrapper';
 
 const Root = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
-    <h1>Configaro</h1>
-    <p style={{ color: theme.colors.primary }}>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-      commodi delectus esse fuga in necessitatibus nihil rem? Animi, ipsam quos?
-    </p>
-    <button type='button'>sdgdsfh</button>
-    <br />
-    <input type='text' />
+    <MainWrapper>
+      <h1>Configaro</h1>
+      <p style={{ color: theme.colors.primary }}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+        commodi delectus esse fuga in necessitatibus nihil rem? Animi, ipsam
+        quos?
+      </p>
+      <p style={{ color: theme.colors.secondary }}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+        commodi delectus esse fuga in necessitatibus nihil rem? Animi, ipsam
+        quos?
+      </p>
+      <p style={{ color: theme.colors.warning }}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+        commodi delectus esse fuga in necessitatibus nihil rem? Animi, ipsam
+        quos?
+      </p>
+      <p style={{ color: theme.colors.success }}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+        commodi delectus esse fuga in necessitatibus nihil rem? Animi, ipsam
+        quos?
+      </p>
+      <p style={{ color: theme.colors.danger }}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+        commodi delectus esse fuga in necessitatibus nihil rem? Animi, ipsam
+        quos?
+      </p>
+      <button type='button'>sdgdsfh</button>
+      <br />
+      <input type='text' />
+    </MainWrapper>
   </ThemeProvider>
 );
 
--- frontend/src/theme/theme.js
+++ frontend/src/theme/theme.js
@@ -1,6 +1,10 @@
 const theme = {
   colors: {
-    primary: 'blue',
+    primary: '#008BF8',
+    secondary: '#9046CF',
+    danger: '#E12C52',
+    warning: '#F5B700',
+    success: '#36D937',
   },
 };
 

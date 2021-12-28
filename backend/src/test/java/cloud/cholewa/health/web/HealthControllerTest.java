761a8631ac91f68f46dd62651c37f06cf769f1c5
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-12-28 21:47:03 +0100

    [CONF-48] add fake test

---
+++ backend/src/test/java/cloud/cholewa/health/web/HealthControllerTest.java
@@ -0,0 +1,16 @@
+package cloud.cholewa.health.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HealthControllerTest {
+    @Test
+    public void shouldGiveFakeTest() {
+        //given
+
+        //when
+        //then
+        assertTrue(true);
+    }
+}

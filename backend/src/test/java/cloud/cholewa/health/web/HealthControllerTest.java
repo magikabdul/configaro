15c7439243270f5c2d5e7e826bf1b1a89848a766
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-12-30 16:28:39 +0100

    [CONF-48] added simple test to check if maven will detect it

--- backend/src/test/java/cloud/cholewa/health/web/HealthControllerTest.java
+++ backend/src/test/java/cloud/cholewa/health/web/HealthControllerTest.java
@@ -2,9 +2,10 @@ package cloud.cholewa.health.web;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HealthControllerTest {
+
     @Test
     public void shouldGiveFakeTest() {
         //given
